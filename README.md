b9963c4850e4a0466742b4bc9dbbec28fee2a707
Tom <tomvercaut@gmail.com>
2024-01-05 10:38:44 +0100

    Add `ls_rtplan` tool and its installation script
    Added a new tool `ls_rtplan` which lists RTPLAN DICOM files in a directory. An installation script was also created to convert the tool into a standalone executable. The tool uses `argparse`, `os`, `time`, `pathlib` and `pydicom` libraries for its operations.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rtpytools
+
+This Python project is a collection of tools to handle and manipulate radiotherapy-related data.
+
+## Tools
+
+- [ls_rtplan](ls_rtplan/README.md): commandline application to list RTPLAN DICOM files. More info 
---
+++ ls_rtplan/install.py
@@ -0,0 +1,23 @@
+import PyInstaller.__main__
+
+
+def run_pyinstaller(path: str):
+    """
+    Run PyInstaller to convert a Python script into a standalone executable file.
+
+    :param path: The path to the Python script file.
+    :return: None
+
+    Example Usage:
+    run_pyinstaller('path/to/my_script.py')
+    """
+    PyInstaller.__main__.run([
+        '--hiddenimport=pydicom.encoders.gdcm',
+        '--hiddenimport=pydicom.encoders.pylibjpeg',
+        '--onefile', path
+    ])
+
+
+if __name__ == "__main__":
+    script_path = 'ls_rtplan.py'
+    run_pyinstaller(script_path)
